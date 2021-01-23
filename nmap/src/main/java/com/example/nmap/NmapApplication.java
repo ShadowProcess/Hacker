3c1aec0292059e6185f6426d17abe4a5369b113f
alex <jiahangsocket@163.com>
2021-01-23 17:10:05 +0800

    first commit

---
+++ nmap/src/main/java/com/example/nmap/NmapApplication.java
@@ -0,0 +1,13 @@
+package com.example.nmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NmapApplication.class, args);
+    }
+
+}
---
+++ nmap/src/test/java/com/example/nmap/NmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
